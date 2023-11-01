edecc0a88bfcd15383cedd1e49c3613a486a237b
LadyCari <k_roferraris@hotmail.com>
2023-10-31 21:20:51 -0300

    casi terminado

---
+++ js.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+        const formulario = document.querySelector('form');
+        const cantidadInput = formulario.querySelector('#cantidad');
+        const categoriaSelect = formulario.querySelector('#categoria');
+        const totalPago = document.getElementById('totalPago');
+
+        formulario.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const cantidad = parseFloat(cantidadInput.value);
+            const categoria = categoriaSelect.value;
+
+            
+            const descuentos = {
+                estudiante: 0.8,
+                trainee: 0.5,
+                junior: 0.15,
+            };
+
+            const precioTicket = 200;
+            const descuento = descuentos[categoria];
+            const descontar = cantidad * precioTicket * descuento;
+            const total = precioTicket - descontar;
+
+            if (!isNaN(total)){
+                totalPago.textContent = total.toFixed(2); 
+            } else{
+                alert('Error no ingreso la cantidad de tickets');
+            }
+
+            
+        });
+    });
